b4a217df4f0e98ec4d8e9ad2d8306640de53ac00
Sebastian Łużyński <sebastian.luzynski@gmail.com>
2021-12-05 19:44:14 +0100

    Day 4 solution
    This one was tough

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,40 +1,72 @@
 use std::{io::{BufReader, BufRead, Lines}, fs::File};
-
-struct BoardElement {
-    number: usize,
-    was_called: bool
-}
-
-impl BoardElement {
-    fn new(number: usize) -> Self {
-        BoardElement {
-            number,
-            was_called: false
-        }
-    }
-}
+mod structs;
+use structs::{Board, BoardElement};
 
 pub fn day4_part1(reader: BufReader<File>) -> usize {
     let mut lines = reader.lines();
     let input_string = lines.next().unwrap().unwrap();
     let numbers_to_call: Vec<usize> = input_string.split(',').map(|n| n.parse().unwrap()).collect();
-    let mut board_elements = read_tables(&mut lines);
+    let board_elements = read_tables(&mut lines);
     let number_of_boards = board_elements.len() / 25;
-    
+
+    let mut boards: Vec<Board> = Vec::new();
+    for i in 0..number_of_boards {
+        let mut temporary_board_elemets = Vec::new();
+        temporary_board_elemets.extend_from_slice(&board_elements[(25*i)..(25*i+25)]);
+        boards.push(Board::new(temporary_board_elemets));
+    }
+
     for called_number in numbers_to_call.iter() {
-        for board in 0..number_of_boards {
-            for i in 0..25 {
-                let board_element = &mut board_elements[25*board + i];
-                if board_element.number == *called_number {
-                    board_element.was_called = true;
-                }
+        for board in boards.iter_mut() {
+            board.check_number(*called_number);
+            if board.check_for_win() {
+                let score = board.calculate_score();
+                return score * called_number;
             }
         }
     }
 
-    check_for_wins(&board_elements, number_of_boards);
+    0 // No winning board
+}
 
-    0
+pub fn day4_part2(reader: BufReader<File>) -> usize {
+    let mut lines = reader.lines();
+    let input_string = lines.next().unwrap().unwrap();
+    let numbers_to_call: Vec<usize> = input_string.split(',').map(|n| n.parse().unwrap()).collect();
+    let board_elements = read_tables(&mut lines);
+    let number_of_boards = board_elements.len() / 25;
+
+    let mut boards: Vec<Board> = Vec::new();
+    for i in 0..number_of_boards {
+        let mut temporary_board_elemets = Vec::new();
+        temporary_board_elemets.extend_from_slice(&board_elements[(25*i)..(25*i+25)]);
+        boards.push(Board::new(temporary_board_elemets));
+    }
+
+    for called_number in numbers_to_call.iter() {
+        let mut indicies_to_delete = Vec::new();
+        for i in 0..boards.len() {
+            let board = &mut boards[i];
+            board.check_number(*called_number);
+            if board.check_for_win() {
+                board.has_won = true;
+                indicies_to_delete.push(i);
+            }
+        }
+        if boards.len() == 1 {
+            if boards[0].has_won {
+                return called_number * boards[0].calculate_score();
+            }
+            continue;
+        }
+        indicies_to_delete.sort();
+        indicies_to_delete.reverse();
+        for idx in indicies_to_delete {
+            boards.remove(idx);
+        }
+    }
+
+    0 // No winning board
 }
 
 fn read_tables(lines: &mut Lines<BufReader<File>>) -> Vec<BoardElement> {
@@ -57,17 +89,11 @@ fn read_tables(lines: &mut Lines<BufReader<File>>) -> Vec<BoardElement> {
     }
 }
 
-fn check_for_wins(board_elements: &Vec<BoardElement>, number_of_boards: usize) {
-    for i in 0..5 {
-        
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::{fs::File, io::BufReader};
 
-    use crate::day4_part1;
+    use crate::{day4_part1, day4_part2};
 
     #[test]
     fn basic_example_part1() {
@@ -76,5 +102,32 @@ mod tests {
         let score = day4_part1(reader);
 
         assert_eq!(score, 4512);
+    }
+
+    #[test]
+    fn basic_example_part2() {
+        let file = File::open("example_input.txt").unwrap();
+        let reader = BufReader::new(file);
+        let score = day4_part2(reader);
+
+        assert_eq!(score, 1924);
+    }
+
+    #[test]
+    fn day4_part1_test() {
+        let file = File::open("input.txt").unwrap();
+        let reader = BufReader::new(file);
+        let score = day4_part1(reader);
+
+        assert_eq!(score, 28082);
+    }
+
+    #[test]
+    fn day4_part2_test() {
+        let file = File::open("input.txt").unwrap();
+        let reader = BufReader::new(file);
+        let score = day4_part2(reader);
+
+        assert_eq!(score, 8224);
     }
 }
---
+++ day4/src/structs.rs
@@ -0,0 +1,81 @@
+#[derive(Debug)]
+pub struct BoardElement {
+    pub number: usize,
+    pub is_marked: bool
+}
+
+impl BoardElement {
+    pub fn new(number: usize) -> Self {
+        BoardElement {
+            number,
+            is_marked: false
+        }
+    }
+}
+
+impl Clone for BoardElement {
+    fn clone(&self) -> Self {
+        Self { number: self.number.clone(), is_marked: self.is_marked.clone() }
+    }
+}
+
+#[derive(Debug)]
+pub struct Board {
+    pub elements: Vec<BoardElement>,
+    pub has_won: bool,
+}
+
+impl Board {
+    pub fn new(numbers: Vec<BoardElement>) -> Self {
+        Board {
+            elements: numbers,
+            has_won: false
+        }
+    }
+
+    pub fn check_number(&mut self, number: usize) {
+        for element in self.elements.iter_mut() {
+            if element.number == number {
+                element.is_marked = true;
+                return;
+            }
+        }
+    }
+
+    pub fn check_for_win(&self) -> bool {
+        'column: for i in 0..5 {
+            for j in 0..5 {
+                let index = i + 5*j;
+                if !self.elements[index].is_marked {
+                    continue 'column;
+                }
+            }
+
+            return true;
+        }
+
+        'row: for i in 0..5 {
+            for j in 0..5 {
+                let index = 5*i + j;
+                if !self.elements[index].is_marked {
+                    continue 'row;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn calculate_score(&self) -> usize {
+        let mut score = 0;
+        for element in self.elements.iter() {
+            if !element.is_marked {
+                score += element.number;
+            }
+        }
+
+        score
+    }
+}
